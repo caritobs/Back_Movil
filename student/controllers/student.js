01ddf53fe4d4db4b669858e148114614aa1682e8
Diana-PC <carolinabravo946@gmail.com>
2024-11-27 02:51:13 -0500

    back listo

--- student/controllers/student.js
+++ student/controllers/student.js
@@ -51,6 +51,8 @@ async function getBrigadasDisponibles(req, res) {
 
 }
 
+//funcion para seleccionar una brigada
+
 async function selectBrigadas(req, res) {
   const { usuario_id, brigada_ids, periodoAcademico } = req.body;
 
@@ -142,7 +144,211 @@ async function selectBrigadas(req, res) {
 }
 
 
+//funcion para que un usuario ueda ver sus brigadas seleccionadas
+
+async function getUsuariosBrigadas(req, res) {
+  const { usuario_id } = req.params;
+
+  if (!usuario_id) {
+    return res.status(400).json({ error: 'El usuario es requerido.' });
+  }
+
+  try {
+    // Utilizar la conexión a la base de datos ya existente
+    const db = dbConection.get();
+    const usuariosCollection = db.db('Usuarios').collection('data');
+    const brigadasCollection = db.db('Brigadas').collection('data');
+
+    // Verificar si el usuario existe
+    const usuario = await usuariosCollection.findOne({ usuario_id });
+
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no registrado' });
+    }
+
+    if (!usuario.brigadas || usuario.brigadas.length === 0) {
+      return res.status(404).json({ error: 'El usuario no está registrado en ninguna brigada' });
+    }
+
+    // Obtener información de las brigadas a las que pertenece el usuario
+    const brigadas = await brigadasCollection.find({ brigada_id: { $in: usuario.brigadas } }).project({ nombre: 1, actividad: 1, diaSemana: 1, _id: 0 }).toArray();
+
+    res.status(200).json(brigadas);
+  } catch (error) {
+    console.error('Error al obtener las brigadas del usuario:', error);
+    res.status(500).json({ error: 'Error al obtener las brigadas del usuario' });
+  }
+}
+
+
+//funcion ppara ver los estudiantes que pertenecen a una brigada:
+async function getBrigadaEstudiantes(req, res) {
+  const { brigada_id, periodoAcademico } = req.query;
+
+  if (!brigada_id || !periodoAcademico) {
+    return res.status(400).json({ error: 'Brigada y periodo académico son requeridos.' });
+  }
+
+  try {
+    // Utilizar la conexión a la base de datos ya existente
+    const db = dbConection.get();
+    const brigadasCollection = db.db('Brigadas').collection('data');
+
+    // Verificar si la brigada existe y obtener los usuarios registrados
+    const brigada = await brigadasCollection.findOne({ brigada_id, periodoAcademico });
+
+    if (!brigada) {
+      return res.status(404).json({ error: 'La brigada no existe para el periodo académico indicado.' });
+    }
+   
+
+    const usuariosCollection = db.db('Usuarios').collection('data');
+    const estudiantes = await usuariosCollection.find({ usuario_id: { $in: brigada.usuarios } }).project({ nombre: 1, usuario_id: 1, _id: 0 }).toArray();
+
+    if (estudiantes.length === 0) {
+      return res.status(404).json({ error: 'No existen estudiantes registrados en esta brigada.' });
+    }
+
+    res.status(200).json(estudiantes);
+  } catch (error) {
+    console.error('Error al obtener los estudiantes de la brigada:', error);
+    res.status(500).json({ error: 'Error al obtener los estudiantes de la brigada' });
+  }
+}
+
+async function completarTarea(req, res) {
+  const { tarea_id, asistentes, evidencia, observacion } = req.body;
+
+  if (!tarea_id || !asistentes || !Array.isArray(asistentes) || !observacion) {
+    return res.status(400).json({ error: 'Tarea, asistentes (array), evidencia y descripción son requeridos.' });
+  }
+
+  try {
+    // Utilizar la conexión a la base de datos ya existente
+    const db = dbConection.get();
+    const tareasCollection = db.db('Tareas').collection('data');
+
+    // Verificar si la tarea existe
+    const tarea = await tareasCollection.findOne({ tarea_id });
+
+    if (!tarea) {
+      return res.status(404).json({ error: 'La tarea no existe.' });
+    }
+
+    // Permitir completar la tarea solo si el estado es 'por completar'
+    if (tarea.estado !== 'por completar') {
+      return res.status(400).json({ error: 'La tarea no está en estado para completar.' });
+    }
+
+    // Actualizar la tarea con los asistentes, la evidencia y la observación, y marcarla como completada
+    await tareasCollection.updateOne(
+      { tarea_id },
+      {
+        $set: {
+          asistentes: asistentes,
+          evidencia_id: evidencia,
+          observacion: observacion,
+          estado: 'completada'
+        }
+      }
+    );
+
+    res.status(200).json({ message: 'Tarea completada exitosamente' });
+  } catch (error) {
+    console.error('Error al completar la tarea:', error);
+    res.status(500).json({ error: 'Error al completar la tarea' });
+  }
+}
+
+async function getTareasPorBrigada(req, res) {
+  const { usuario_id, periodoAcademico } = req.query;
+
+  if (!usuario_id || !periodoAcademico) {
+    return res.status(400).json({ error: 'Usuario y periodo académico son requeridos.' });
+  }
+
+  try {
+    // Utilizar la conexión a la base de datos ya existente
+    const db = dbConection.get();
+    const usuariosCollection = db.db('Usuarios').collection('data');
+    const tareasCollection = db.db('Tareas').collection('data');
+
+    // Verificar si el usuario existe
+    const usuario = await usuariosCollection.findOne({ usuario_id });
+
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no registrado' });
+    }
+
+    if (!usuario.brigadas || usuario.brigadas.length === 0) {
+      return res.status(404).json({ error: 'Debe escoger una brigada para poder ver las tareas.' });
+    }
+
+    // Obtener las tareas asociadas a las brigadas del usuario y filtrar por el periodo académico
+    const tareas = await tareasCollection.find({
+      brigada_id: { $in: usuario.brigadas },
+      periodoAcademico: periodoAcademico
+    }).project({ tarea_id: 1, brigada_id: 1, descripcion: 1, fecha: 1, estado: 1, _id: 0 }).toArray();
+
+    res.status(200).json(tareas);
+  } catch (error) {
+    console.error('Error al obtener las tareas de la brigada del usuario:', error);
+    res.status(500).json({ error: 'Error al obtener las tareas de la brigada del usuario' });
+  }
+}
+
+// Función para obtener la información completa de una tarea ya completada
+async function getTareaCompletada(req, res) {
+  const { tarea_id } = req.params;
+
+  if (!tarea_id) {
+    return res.status(400).json({ error: 'El ID de la tarea es requerido.' });
+  }
+
+  try {
+    // Utilizar la conexión a la base de datos ya existente
+    const db = dbConection.get();
+    const tareasCollection = db.db('Tareas').collection('data');
+    const usuariosCollection = db.db('Usuarios').collection('data');
+
+    // Verificar si la tarea existe y ha sido completada
+    const tarea = await tareasCollection.findOne({ tarea_id, estado: 'completada' });
+
+    if (!tarea) {
+      return res.status(404).json({ error: 'La tarea no existe o no está completada.' });
+    }
+
+    // Obtener información de los asistentes (solo los que asistieron)
+    const asistentesInfo = await usuariosCollection.find({ usuario_id: { $in: tarea.asistentes } }).project({ nombre: 1, usuario_id: 1, _id: 0 }).toArray();
+
+    // Armar la respuesta con la información detallada de la tarea
+    const tareaCompleta = {
+      tarea_id: tarea.tarea_id,
+      brigada_id: tarea.brigada_id,
+      descripcion: tarea.descripcion,
+      observacion: tarea.observacion,
+      fecha: tarea.fecha,
+      estado: tarea.estado,
+      asistentes: asistentesInfo,
+      evidencia: tarea.evidencia_id // Imagen en Base64
+      
+    };
+
+    res.status(200).json(tareaCompleta);
+  } catch (error) {
+    console.error('Error al obtener la información de la tarea completada:', error);
+    res.status(500).json({ error: 'Error al obtener la información de la tarea completada' });
+  }
+}
+
+
+
+
+
+
+
 
 module.exports = {
-  getBrigadas, getBrigadasDisponibles, selectBrigadas
+  getBrigadas, getBrigadasDisponibles, selectBrigadas, getUsuariosBrigadas, completarTarea,getTareasPorBrigada,getBrigadaEstudiantes
+  ,getTareaCompletada
 }
--- student/routes/routes.js
+++ student/routes/routes.js
@@ -9,5 +9,19 @@ router.get('/brigadas/disponibles', studentController.getBrigadasDisponibles);
 // Ruta para seleccionar brigadas
 router.post('/brigadas/seleccionar', studentController.selectBrigadas);
 
+//ruta para ver las brigadas a la sque pertenece un usuario
+router.get('/usuarios/:usuario_id/brigadas', studentController.getUsuariosBrigadas);
+
+// Ruta para obtener los estudiantes de una brigada específica
+router.get('/brigada/estudiantes', studentController.getBrigadaEstudiantes);
+
+// Ruta para completar una tarea
+router.post('/tareas/completar', studentController.completarTarea);
+
+// Ruta para obtener las tareas de las brigadas del usuario en el periodo académico indicado
+router.get('/tareas/brigada', studentController.getTareasPorBrigada);
+
+//ruta para ver una tarea completada
+router.get('/tareas/completada/:tarea_id', studentController.getTareaCompletada);
 
 module.exports = router;
